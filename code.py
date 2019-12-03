be700caaad07423ad3acd7c34cf7a32a38dc68b2
maddie064 <33437196+maddie064@users.noreply.github.com>
2019-12-02 19:44:38 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+def countdown ():
+    for i in range(10,0,-1):
+        print(i)
+
+def madison ():
+    for i in range(5):
+        print("madison")
+
+def wow ():
+    for i in range(10):
+        print("teal"+ str(i))
+
+def yesssss (x):
+    for i in range(x):
+        countdown()
+        madison()
+        wow()
